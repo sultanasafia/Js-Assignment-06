afba010419da796f78dba39a38cb0b276fa652b7
safia bano <safiabano736@gmail.com>
2024-08-20 17:43:18 +0500

    asignment6

---
+++ app.js
@@ -0,0 +1,45 @@
+// Chap 6 start 
+
+// Q1
+
+// let a = 10;
+
+// document.write("The value of a is "+ a+ "<br>")
+// document.write("-------------------------"+ "<br>")
+// ++a
+// document.write("The value of ++a is :"+ a+ "<br>"+ "<br>")
+// document.write("Now the value of a is :"+ a+ "<br>"+ "<br>")
+// document.write("Now the value of a++ is :"+ a+ "<br>"+ "<br>")
+// a++
+// document.write("Now the value of a is :"+ a+ "<br>"+ "<br>")
+// --a
+// document.write("Now the value of --a is :"+ a+ "<br>"+ "<br>")
+
+// document.write("Now the value of a is :"+ a+ "<br>"+ "<br>")
+// document.write("Now the value of a-- is :"+ a+ "<br>"+ "<br>")
+// a--
+// document.write("Now the value of a is :"+ a+ "<br>"+ "<br>")
+
+// Q2
+
+// let a = 2;
+// let b = 1;
+// let result = --a - --b + ++b + b--;
+// console.log(--a);
+// console.log(--a - --b + ++b);
+// console.log(--a - --b + ++b + b--);
+
+// Q3
+
+// let userName = prompt("Enter your name" , "Ali")
+// alert("Welcome "+ userName)
+
+// Q4
+
+// let table =prompt("Enter your table")
+// for(let i = 1; i <= 10; i++){
+//     document.write(table + " x "+ i + " = "+ table*i +"<br>");
+// }
+// Q5
+
+// mention in html file
